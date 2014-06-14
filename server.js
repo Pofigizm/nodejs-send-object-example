65e64a21bebefa52447590727788a897155c1f8d
Denis Iogansen <pofigizm@gmail.com>
2014-06-15 02:24:43 +0400

    create commit
    create commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function ClassOrder(name, cost) {
+  this.name = name;
+  this.cost = cost;
+  this.parts = [];
+  this.summ = function() {
+    var summ = 0;
+    this.parts.forEach(function(part) {
+      summ += part.summ();
+    });
+    return summ + this.cost;
+  };
+}
+
+var myobject = new ClassOrder('my object', 10);
+myobject.parts[0] = new ClassOrder('my first part', 20);
+myobject.parts[1] = new ClassOrder('my second part', 30);
+myobject.parts[1].parts[0] = new ClassOrder('my first in second part', 40);
+
+console.log(myobject);
+console.log(myobject.summ());
+
+http.createServer(function(req, res) {
+  switch(req.url) {
+    case '/':
+      sendFile("index.html", res);
+      break;
+    case '/getobject':
+      res.end(JSON.stringify(myobject));
+      break;
+    default:
+      res.statusCode = 404;
+      res.end("Page not found!");
+  }
+}).listen(80);
+
+function sendFile(fileName, res) {
+  var fileStream = fs.createReadStream(fileName);
+  fileStream
+    .on('error', function () {
+      res.statusCode = 500;
+      res.end("Server error");
+    })
+    .pipe(res)
+    .on('close', function () {
+      fileStream.destroy();
+    });
+}
